709e92c092ccc9e0d21556d65a9f9e52d2b6d614
Daniel Ceriana <danielcerianax@gmail.com>
2023-07-25 18:26:02 -0300

    [FEAT] agregada logica de fin de juego

--- JS/factory-functions-and-module-pattern/proyect-TicTacToe/script/index.js
+++ JS/factory-functions-and-module-pattern/proyect-TicTacToe/script/index.js
@@ -1,5 +1,11 @@
 //name of the player
 const player = (name) => {
+    let playerName;
+
+    function validateString() {
+
+    }
+
     return { name }
 }
 
@@ -21,14 +27,82 @@ const GameBoard = (() => {
             }
         }
 
-        console.log(gameBoard);
 
     }
 
-    //checks gameOver condition 
-    function gameOver() {
-
+    let checkEquals = (first, middle, last) => {
+        let returnVal = false;
+        if (first === middle && first === last) {
+            returnVal = true;
+        } else {
+            returnVal = false;
+        }
+        return returnVal;
     }
+
+    //checks gameOver condition  0 is not over, 1 player won, 2 tie
+    function isGameOver() {
+
+        let gameBoardArray = [];
+        let returnVal = 0;
+        let arrayLength;
+        gameBoard.forEach(row => {
+            row.forEach(column => {
+                gameBoardArray.push(column);
+            })
+
+
+        });
+        arrayLength = gameBoardArray.length;
+        if (arrayLength > 0) {
+
+            for (let i = 0; i < 3; i++) {
+                if (checkEquals(gameBoardArray[i], gameBoardArray[i + 3], gameBoardArray[i + 6])) {
+                    returnVal = 1;
+                    break;
+                }
+            }
+            // [0][3][6]
+            // [1][4][7]
+            // [2][5][8]
+            if (returnVal == 0) {
+                for (let i = 0; i < 5; i += 3) {
+                    if (checkEquals(gameBoardArray[i], gameBoardArray[i + 1], gameBoardArray[i + 2])) {
+                        returnVal = 1;
+                        break;
+                    }
+                }
+            }
+            // [0][1][2]
+            // [3][4][5]
+            // [6][7][8]
+
+            if (returnVal == 0) {
+                if (checkEquals(gameBoardArray[0], gameBoardArray[4], gameBoardArray[8])) {
+                    returnVal = 1;
+                }
+
+                if (checkEquals(gameBoardArray[2], gameBoardArray[4], gameBoardArray[6])) {
+                    returnVal = 1;
+                }
+            }
+            // [0][4][8]
+            // [2][4][6]
+            if (returnVal == 0) {
+                for (let i = 0; i < arrayLength; i++) {
+                    if (gameBoardArray[i] != 'X' && gameBoardArray[i] != 'O') {
+                        returnVal = 0;
+                        break;
+                    } else {
+                        returnVal = 2;
+                    }
+                }
+            }
+        }
+        return returnVal;
+    }
+
+
 
     // resets tiles
     function gameReset() {
@@ -43,7 +117,10 @@ const GameBoard = (() => {
             }
         }
 
+
     }
+
+
 
     function gameBoardChange(i, j, value) {
         gameBoard[i][j] = value
@@ -59,7 +136,7 @@ const GameBoard = (() => {
     }
 
 
-    return { gameStart, gameOver, gameReset, gameBoardChange, gameBoard, gameBoardIsTileAvailable }
+    return { gameStart, isGameOver, gameReset, gameBoardChange, gameBoard, gameBoardIsTileAvailable }
 })();
 
 const DisplayController = () => {
@@ -67,14 +144,45 @@ const DisplayController = () => {
     const buttonsLength = gameButtons.length;
     const gameBoardArray = [];
     const turnLabel = document.querySelector(".turn");
-    const playerCount = document.querySelector(".player-count");
-    const playerTwoInput = document.querySelector("#player-2-name")
+    // const playerCount = document.querySelector(".player-count");
+    const playerCountRadios = document.querySelectorAll(".player-count");
+    const playerTwoInput = document.querySelector("#player-2-name");
+    const playerOneWins = document.querySelector(".player-1-wins");
+    const playerTwoWins = document.querySelector(".player-2-wins");
+
+    function addPlayerWin(player) {
+        let playerWins;
+
+        if (player !== NaN && player == "player-1") {
+            try {
+                playerWins = Number(playerOneWins.textContent);
+                playerWins++;
+                playerOneWins.textContent = playerWins;
+            } catch (error) {
+                console.log(error)
+            }
 
 
-    playerCount.addEventListener("click", (e) => {
-        console.log("ALGO ESTA PASANDO")
-        checkNumberOfPlayers();
-    })
+        } else {
+            playerWins = Number(playerTwoWins.textContent);
+            playerWins++;
+            playerTwoWins.textContent = playerWins;
+        }
+        for (let i = 0; i < buttonsLength; i++) {
+            gameButtons[i].disabled = true;
+        }
+    }
+
+    function resetPlayerWins() {
+        playerOneWins.textContent = 0;
+        playerTwoWins.textContent = 0;
+    }
+
+    playerCountRadios.forEach(radio => {
+        radio.addEventListener("click", (e) => {
+            checkNumberOfPlayers();
+        })
+    });
 
     function gameRender(gameBoard, start = 0) {
         const gameBoardLength = gameBoard.length;
@@ -92,6 +200,7 @@ const DisplayController = () => {
         }
     }
 
+    //just changes one tile
     function playerClickRender(tile, value) {
 
         gameButtons[tile].textContent = value;
@@ -101,20 +210,25 @@ const DisplayController = () => {
         turnLabel.textContent = turn;
     }
 
+
     function checkNumberOfPlayers() {
-        if (playerCount.value == 2) {
-            playerTwoInput.disabled = false;
-        } else {
+        let returnVal = 0;
+        if (playerCountRadios[0].checked == true) {
+            returnVal = 1;
             playerTwoInput.disabled = true;
+        } else {
+            returnVal = 2;
+            playerTwoInput.disabled = false;
         }
-        return playerCount.value;
+        return returnVal;
     }
 
-    return { gameRender, playerClickRender, displayTurn, checkNumberOfPlayers };
+    return { gameRender, playerClickRender, displayTurn, checkNumberOfPlayers, addPlayerWin, resetPlayerWins };
 }
 
 
 //IIFE
+// game flow logic
 const Game = (() => {
     const gameButtons = document.querySelectorAll(".game-button");
     const startButton = document.querySelector(".start-button")
@@ -138,21 +252,25 @@ const Game = (() => {
 
     //event to start the game with logic for number of players
     startButton.addEventListener("click", () => {
+        resetTurn();
         gameBoard.gameStart();
         displayController.gameRender(gameBoard.gameBoard, 0);
 
         //player names
-        if (displayController.checkNumberOfPlayers == 2) {
+        if (displayController.checkNumberOfPlayers() == 2) {
             if (playerNameIsValid(playerTwoInput.value)) {
                 playerTwo.name = playerTwoInput.value;
+                console.log(playerTwoInput.value)
             }
 
         }
+
         if (playerNameIsValid(playerOneInput.value)) {
             playerOne.name = playerOneInput.value
         }
-
+        displayController.displayTurn(playerOne.name);
     })
+
 
     function playerNameIsValid(name) {
         if (name.length > 0) {
@@ -164,8 +282,11 @@ const Game = (() => {
 
     // event to set every tile and player names to default
     restartButton.addEventListener("click", () => {
+        resetTurn();
+        displayController.displayTurn(playerOne.name);
         gameBoard.gameReset();
         displayController.gameRender(gameBoard.gameBoard, 1);
+        displayController.resetPlayerWins();
     })
 
     gameButtons.forEach(tile => {
@@ -176,6 +297,7 @@ const Game = (() => {
 
     function playerClickHandler(tile, player) {
         let h = 0
+        let asd;
         for (let i = 0; i < 3; i++) {
             for (let j = 0; j < 3; j++) {
                 if (gameBoard.gameBoardIsTileAvailable(i, j)) {
@@ -189,6 +311,13 @@ const Game = (() => {
                             displayController.playerClickRender(tile, "O");
                             gameBoard.gameBoardChange(i, j, "O");
                         }
+                        asd = gameBoard.isGameOver();
+                        if (asd == 1) {
+                            displayController.addPlayerWin(turn);
+                        } else if (asd == 2) {
+                            console.log("Tie");
+                        };
+
                         nextTurn();
                     }
 
@@ -196,6 +325,10 @@ const Game = (() => {
                 h++;
             }
         }
+    }
+
+    function resetTurn() {
+        turn = "player-1";
     }
 
     function nextTurn() {
